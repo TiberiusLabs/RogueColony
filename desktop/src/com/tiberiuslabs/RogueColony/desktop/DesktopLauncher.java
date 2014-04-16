1d9b6558c049edcbc2688a3906d56d65037ca58e
Aman Gill <ancillaryman@gmail.com>
2014-04-16 14:10:52 -0700

    created empty libGDX project

---
+++ desktop/src/com/tiberiuslabs/RogueColony/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tiberiuslabs.RogueColony.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tiberiuslabs.RogueColony.RogueColonyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RogueColonyGame(), config);
+	}
+}
